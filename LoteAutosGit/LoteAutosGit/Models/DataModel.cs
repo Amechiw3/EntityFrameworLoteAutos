bb03483bddd88a97ae1cba32a88b977326081b5c
Amechiw3 <martinfierro97@hotmail.com>
2017-02-11 16:01:02 -0700

    DataModel

---
+++ LoteAutosGit/LoteAutosGit/Models/DataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  USING  */
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace LoteAutosGit.Models
+{
+    class DataModel : DbContext
+    {
+        public DataModel() : base("DataModel")  {   }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
