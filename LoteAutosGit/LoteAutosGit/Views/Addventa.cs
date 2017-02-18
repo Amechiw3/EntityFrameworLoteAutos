1a83bbd7ac2b8eb4adefb9c44f0bc0175fa2d3f6
alice mariscal <alicia_osbourne@hotmail.com>
2017-02-18 04:36:28 -0700

    aliii

--- LoteAutosGit/LoteAutosGit/Views/Addventa.cs
+++ LoteAutosGit/LoteAutosGit/Views/Addventa.cs
@@ -8,13 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LoteAutosGit
+namespace LoteAutosGit.Views
 {
-    public partial class Addventa : Form
+    public partial class AddVenta : Form
     {
-        public Addventa()
+        public AddVenta()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
