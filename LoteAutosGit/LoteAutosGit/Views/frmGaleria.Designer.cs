ceedef0f2d0ef8c66e821d9ab36a04465ddac609
Amechiw3 <martinfierro97@hotmail.com>
2017-02-18 04:47:08 -0700

    galeria

---
+++ LoteAutosGit/LoteAutosGit/Views/frmGaleria.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace LoteAutosGit.Views
+{
+    partial class frmGaleria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbFoto = new System.Windows.Forms.PictureBox();
+            this.btnTomar = new System.Windows.Forms.Button();
+            this.btnExaminar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbFoto
+            // 
+            this.pcbFoto.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pcbFoto.Location = new System.Drawing.Point(13, 14);
+            this.pcbFoto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pcbFoto.Name = "pcbFoto";
+            this.pcbFoto.Size = new System.Drawing.Size(339, 152);
+            this.pcbFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pcbFoto.TabIndex = 0;
+            this.pcbFoto.TabStop = false;
+            // 
+            // btnTomar
+            // 
+            this.btnTomar.Location = new System.Drawing.Point(64, 174);
+            this.btnTomar.Name = "btnTomar";
+            this.btnTomar.Size = new System.Drawing.Size(119, 33);
+            this.btnTomar.TabIndex = 2;
+            this.btnTomar.Text = "Tomar foto";
+            this.btnTomar.UseVisualStyleBackColor = true;
+            this.btnTomar.Click += new System.EventHandler(this.btnTomar_Click);
+            // 
+            // btnExaminar
+            // 
+            this.btnExaminar.Location = new System.Drawing.Point(189, 174);
+            this.btnExaminar.Name = "btnExaminar";
+            this.btnExaminar.Size = new System.Drawing.Size(119, 33);
+            this.btnExaminar.TabIndex = 4;
+            this.btnExaminar.Text = "Examinar";
+            this.btnExaminar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(12, 259);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(136, 47);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(217, 259);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(136, 47);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmGaleria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 318);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnExaminar);
+            this.Controls.Add(this.btnTomar);
+            this.Controls.Add(this.pcbFoto);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmGaleria";
+            this.Text = "frmGaleria";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbFoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbFoto;
+        private System.Windows.Forms.Button btnTomar;
+        private System.Windows.Forms.Button btnExaminar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
