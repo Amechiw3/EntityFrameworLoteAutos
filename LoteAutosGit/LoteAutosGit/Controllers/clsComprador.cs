bc6a7e5b189d073b7e8f735d436cfd0ce4a3d393
EvelinMartinez <evelin_fda@hotmail.com>
2017-02-18 03:54:44 -0700

    comprador

--- LoteAutosGit/LoteAutosGit/Controllers/clsComprador.cs
+++ LoteAutosGit/LoteAutosGit/Controllers/clsComprador.cs
@@ -5,10 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 using LoteAutosGit.Models;
 using System.Data.Entity;
+using LoteAutosGit.Models;
 
 namespace LoteAutosGit.Controllers
 {
     class clsComprador
     {
+        public static comprador searchIne(string Ine)
+        {
+            try
+            {
+                var ctx = new DataModel();
+                return ctx.compradores.Where(r => r.Ine == Ine).FirstOrDefault();
+            }
+            catch (Exception exc)
+            {
+                throw;
+            }
+        }
+        public static void SaveComprador(comprador ine)
+        {
+            try
+            {
+                var ctx = new DataModel();
+                //propietario datos = clsManejoPropietarios.returnPropietario(propietario);
+                //ctx.propietarios.Attach(datos);
+                //dauto.propietarios = datos;
+
+                if (ine.idcomprador > 0) //Actualizar
+                {
+                    ctx.Entry(ine).State = EntityState.Modified;
+                }
+                else // Agregar
+                {
+                    ctx.Entry(ine).State = EntityState.Added;
+                }
+                ctx.SaveChanges();
+            }
+            catch (Exception exc)
+            {
+
+                throw;
+            }
+        }
     }
 }
