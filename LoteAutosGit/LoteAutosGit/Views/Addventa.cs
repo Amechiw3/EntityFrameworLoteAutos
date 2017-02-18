defca45880b7d68706bb78429c86af0a5534d939
alice mariscal <alicia_osbourne@hotmail.com>
2017-02-18 06:10:08 -0700

    1

--- LoteAutosGit/LoteAutosGit/Views/Addventa.cs
+++ LoteAutosGit/LoteAutosGit/Views/Addventa.cs
@@ -8,9 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LoteAutosGit.Views;
-   
+namespace LoteAutosGit.Views
 
+{ 
     public partial class AddVenta : Form
     {
 
@@ -22,6 +22,21 @@ namespace LoteAutosGit.Views;
 
         private void button2_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+       // {
+           // try
+          //  {
+               // propietario datosprop = clsManejoPropietarios.returnPropietario(int.Parse(cboPropierario.SelectedValue.ToString()));
+               // idpropietario = datosprop.idpropietario;
+              //  txtNombre.Text = datosprop.nombre;
+           // }
+           // catch (Exception)
+         /   {
+
+            }
 
         }
     }
