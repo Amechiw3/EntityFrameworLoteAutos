025e33da0fa25366c3a00ebf453e2ff8939a3f68
EvelinMartinez <evelin_fda@hotmail.com>
2017-02-26 00:28:55 -0700

    comp

--- LoteAutosGit/LoteAutosGit/Program.cs
+++ LoteAutosGit/LoteAutosGit/Program.cs
@@ -18,7 +18,7 @@ namespace LoteAutosGit
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMainSistema());
+            Application.Run(new frmVenta());
         }
     }
 }
