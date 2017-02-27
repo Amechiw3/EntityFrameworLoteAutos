e77f0ef5e435c11404bf1eb97fe9b1e429891a9c
EvelinMartinez <evelin_fda@hotmail.com>
2017-02-27 01:27:38 -0700

    pago

---
+++ LoteAutosGit/LoteAutosGit/View/FrmPagoFinal.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace LoteAutosGit.View
+{
+    partial class FrmPagoFinal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textTotal = new System.Windows.Forms.TextBox();
+            this.txtEfectivo = new System.Windows.Forms.TextBox();
+            this.txtCambio = new System.Windows.Forms.TextBox();
+            this.btbSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota de Venta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Su Pago en Efectivo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Su Cambio:";
+            // 
+            // textTotal
+            // 
+            this.textTotal.Location = new System.Drawing.Point(177, 38);
+            this.textTotal.Name = "textTotal";
+            this.textTotal.Size = new System.Drawing.Size(156, 26);
+            this.textTotal.TabIndex = 4;
+            // 
+            // txtEfectivo
+            // 
+            this.txtEfectivo.Location = new System.Drawing.Point(177, 70);
+            this.txtEfectivo.Name = "txtEfectivo";
+            this.txtEfectivo.Size = new System.Drawing.Size(156, 26);
+            this.txtEfectivo.TabIndex = 5;
+            // 
+            // txtCambio
+            // 
+            this.txtCambio.Location = new System.Drawing.Point(177, 102);
+            this.txtCambio.Name = "txtCambio";
+            this.txtCambio.Size = new System.Drawing.Size(156, 26);
+            this.txtCambio.TabIndex = 6;
+            // 
+            // btbSalvar
+            // 
+            this.btbSalvar.Location = new System.Drawing.Point(234, 156);
+            this.btbSalvar.Name = "btbSalvar";
+            this.btbSalvar.Size = new System.Drawing.Size(94, 33);
+            this.btbSalvar.TabIndex = 7;
+            this.btbSalvar.Text = "Guardar";
+            this.btbSalvar.UseVisualStyleBackColor = true;
+            // 
+            // FrmPagoFinal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(340, 216);
+            this.Controls.Add(this.btbSalvar);
+            this.Controls.Add(this.txtCambio);
+            this.Controls.Add(this.txtEfectivo);
+            this.Controls.Add(this.textTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmPagoFinal";
+            this.Text = "Gracias por su compra!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textTotal;
+        private System.Windows.Forms.TextBox txtEfectivo;
+        private System.Windows.Forms.TextBox txtCambio;
+        private System.Windows.Forms.Button btbSalvar;
+    }
+}
